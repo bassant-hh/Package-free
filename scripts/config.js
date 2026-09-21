9f8a91ce666e9c328a8b075a7140b67b20f7649d
Bassant Hossny <bassanthossny177@gmail.com>
2026-09-21 19:21:07 +0300

    fix: point frontend to production API

--- scripts/config.js
+++ scripts/config.js
@@ -6,4 +6,4 @@ const API_BASE_URL =
     window.API_BASE_URL ||
     (IS_LOCAL
         ? 'http://localhost:8000'
-        : 'https://iti-group-project-server.vercel.app');
+        : 'https://package-free-5s26.vercel.app/');
